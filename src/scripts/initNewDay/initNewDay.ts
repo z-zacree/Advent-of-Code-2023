b817346287611fbafa060982bffd18426b68fe74
Zachary <zacharylim2004@gmail.com>
2023-12-08 02:17:51 +0800

    📝 Update init day script

--- src/scripts/initNewDay/initNewDay.ts
+++ src/scripts/initNewDay/initNewDay.ts
@@ -25,3 +25,5 @@ const newDayPath = `${basePath}/${day}`;
 mkdirSync(newDayPath);
 copyFileSync(`${__dirname}/Puzzle.ts.tpl`, `${newDayPath}/Puzzle.ts`);
 writeFileSync(`${newDayPath}/input.txt`, '');
+writeFileSync(`${newDayPath}/example-1.txt`, '');
+writeFileSync(`${newDayPath}/example-2.txt`, '');
